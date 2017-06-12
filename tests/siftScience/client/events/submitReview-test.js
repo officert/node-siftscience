e097caea48399a4c93633e754a9aa6b27a1859fe
Tim Officer <timothyofficer@gmail.com>
2017-06-12 13:56:49 -0400

    add remaining events methods

---
+++ tests/siftScience/client/events/submitReview-test.js
@@ -0,0 +1,89 @@
+const should = require('should');
+const Promise = require('bluebird');
+const sinon = require('sinon');
+const _ = require('lodash');
+
+let siftScience;
+let v204;
+let sandbox;
+
+before(() => {
+  siftScience = require('lib');
+  v204 = require('lib/client/v204');
+
+  sandbox = sinon.sandbox.create();
+});
+
+describe('siftScience', () => {
+  describe('client', () => {
+    describe('events', () => {
+      describe('submitReview', () => {
+        afterEach(() => {
+          sandbox.restore();
+        });
+
+        const key = '123';
+        let siftScienceClient;
+
+        before('create siftScienceClient', () => {
+          siftScienceClient = siftScience.init(key);
+        });
+
+        describe('when called with nothing', () => {
+          let postStub;
+
+          let response = {};
+
+          before('stub v204.post()', () => {
+            postStub = sandbox.stub(v204, 'post')
+              .returns(Promise.resolve(response));
+          });
+
+          it('should call v204.post()', () => {
+            return siftScienceClient.events.submitReview()
+              .then(result => {
+                should.exist(result);
+                result.should.deepEqual(response);
+
+                postStub.callCount.should.equal(1);
+                postStub.args[0][0].should.equal('/events');
+                postStub.args[0][1].should.deepEqual({
+                  $type: '$submit_review',
+                  $api_key: key
+                });
+              });
+          });
+        });
+
+        describe('when called with data', () => {
+          let data = {
+            foo: 'bar'
+          };
+          let postStub;
+
+          let response = {};
+
+          before('stub v204.post()', () => {
+            postStub = sandbox.stub(v204, 'post')
+              .returns(Promise.resolve(response));
+          });
+
+          it('should call v204.post()', () => {
+            return siftScienceClient.events.submitReview(data)
+              .then(result => {
+                should.exist(result);
+                result.should.deepEqual(response);
+
+                postStub.callCount.should.equal(1);
+                postStub.args[0][0].should.equal('/events');
+                postStub.args[0][1].should.deepEqual(_.extend(data, {
+                  $type: '$submit_review',
+                  $api_key: key
+                }));
+              });
+          });
+        });
+      });
+    });
+  });
+});
